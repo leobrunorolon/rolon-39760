a199f64d8e282b75a974ae838c077ac33c4311db
U550196 <berolon@teco.com.ar>
2023-05-15 20:05:05 -0300

    Practica integradora

---
+++ src/models/carts.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const cartsCollection = 'carts'
+
+// {
+//   "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+//   "price": 695,
+//   "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+//   "category": "jewelery",
+//   "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+//   "stock": 400
+//   "id": 1
+// },
+
+const cartsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    unique: true,
+    require: true
+  },
+  price: {
+    type: Number,
+    require: true
+  },
+  description: {
+    type: String,
+    require: true
+  },
+  category: {
+    type: String,
+    require: true
+  },
+  image: {
+    type: String,
+    require: true
+  },
+  stock: {
+    type: Number,
+    require: true
+  },
+})
+
+export const cartsModel = mongoose.model(cartsCollection, cartsSchema)
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -15,9 +15,17 @@ router.get('/', async (req, res) => {
   // } catch (err) {
   //   res.status(404).send(err);
   // }
+  try {
+    const carts = await cartsModel.find()
+    const isEmpty = carts.lenght === 0;
+    res.send({ result: 'succes', payload: carts })
+    res.render('carts', { carts, isEmpty });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-router.put('/:product', (req, res) => {
+router.post('/', async (req, res) => {
   // FS
   // try {
   //   const newProduct = req.body;
@@ -25,6 +33,13 @@ router.put('/:product', (req, res) => {
   // } catch (err) {
   //   res.status(404).send(err.msg);
   // }
+  try {
+    const newCarts = req.body;
+    const result = await cartsModel.create(newCarts)
+    res.send({ result: 'succes', payload: result });
+  } catch (err) {
+    res.status(500).send(err.msg);
+  }
 });
 
 export default router;
